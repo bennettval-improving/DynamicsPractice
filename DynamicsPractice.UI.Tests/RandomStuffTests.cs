79ea9648a7a120b5f344f4133b6949205311870f
Bennett Valentine <bennett.valentine@improving.com>
2021-08-10 15:55:58 -0500

    initial commit

---
+++ DynamicsPractice.UI.Tests/RandomStuffTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DynamicsPractice.UI.Tests
+{
+    [TestClass]
+    public class RandomStuffTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
